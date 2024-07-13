b6cfa1adfc8d900586fe745d34eeb347f827358c
Nick Downing <nick@ndcode.org>
2024-07-13 16:52:35 +1000

    Temporary support for 24-bit Intel HEX output from as6500 and other assemblers

--- asxxsrc/asmain.c
+++ asxxsrc/asmain.c
@@ -339,6 +339,7 @@ char *argv[];
 				 *   -i   Insert assembler line before input file(s)
 				 *   -n#  Specify number of assembler scanning passes
 				 *   -v   Enable out of range signed / unsigned errors
+				 *   -234 Increase address length to at least n
 				 */
 				case 'i':
 				case 'I':
@@ -360,6 +361,12 @@ char *argv[];
 				case 'v':
 				case 'V':
 					++vflag;
+					break;
+
+				case '2':
+				case '3':
+				case '4':
+					bytesflag = c - '0';
 					break;
 
 				/*
@@ -549,6 +556,8 @@ char *argv[];
 		awg = 0;
 		csn = 0;
 		minit();
+		if (bytesflag > a_bytes)
+			exprmasks(bytesflag);
 		while ((i = nxtline()) != 0) {
 			cp = cb;
 			cpt = cbt;
@@ -3431,6 +3440,9 @@ char *usetxt[] = {
 	"  -i   Insert assembler line before input file(s)",
 	"  -v   Enable out of range signed / unsigned errors",
 	"  -n#  Set the maximum number of 'Pass 2' scans",
+        "  -2   Increase address length to at least 16-bit",
+        "  -3   Increase address length to at least 24-bit",
+        "  -4   Increase address length to at least 32-bit",
 	"Symbols:",
 	"  -a   All user symbols made global",
 	"  -g   Undefined symbols made global",
